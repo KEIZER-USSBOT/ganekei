cdfa0c0e6eca7ac2ed5e8bcf1331764d21ddb901
Keizer <102486467+KEIZER-USSBOT@users.noreply.github.com>
2022-03-29 11:29:02 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -27,7 +27,7 @@ for module_name in ALL_MODULES:
 
 
 LOGS.info(
-    f"💀KARMAN-USERBOT💀 ⚠️ V7.0 [AKTIF KONTOLL]")
+    f"🀄KEIZER-USERBOT🀄 ⚠️ V7.0 [AKTIF MONYET]")
 
 
 if len(argv) not in (1, 3, 4):
