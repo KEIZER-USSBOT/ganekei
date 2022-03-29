c0d3ae5e63e7f07b9d48afcbe7c61d78e9cba6ef
Keizer <102486467+KEIZER-USSBOT@users.noreply.github.com>
2022-03-29 12:44:57 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -198,7 +198,7 @@ YOUTUBE_API_KEY = os.environ.get("YOUTUBE_API_KEY", None)
 RAM_TEKS_KOSTUM = os.environ.get("RAM_TEKS_KOSTUM") or "ㅤ"
 
 # Untuk Melihat Repo
-REPO_NAME = os.environ.get("REPO_NAME") or "💀KARMAN-USERBOT💀"
+REPO_NAME = os.environ.get("REPO_NAME") or "🀄KEIZER-USERBOT🀄"
 
 # DEVS
 DEVS = (
